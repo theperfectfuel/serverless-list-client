a18c0ccd48f9712897d2196cf0e47699c9771ced
theperfectfuel <robert@theperfectfuel.com>
2017-10-15 21:25:24 -0700

    Adding config setup to reference environment variables

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import './Login.css';
+import config from '../config';
 
 export default class extends Component {
     constructor (props) {
