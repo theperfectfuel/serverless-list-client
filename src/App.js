6187e8c587595731d19b8e4cf7a45f018fc5fbe4
theperfectfuel <robert@theperfectfuel.com>
2017-10-16 22:42:40 -0700

    Completed redirect on login / logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { Nav, NavItem, Navbar } from 'react-bootstrap';
 import Routes from './Routes';
 import RouteNavItem from './components/RouteNavItem';
@@ -24,6 +24,7 @@ class App extends Component {
   handleLogout = event => {
     signOutUser();
     this.userHasAuthenticated(false);
+    this.props.history.push('/login');
   }
 
   async componentDidMount() {
@@ -78,4 +79,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
