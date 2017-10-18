bbecc739873c81f2f33f6c362156b9df06d885c0
theperfectfuel <robert@theperfectfuel.com>
2017-10-18 01:04:50 -0700

    Added new note form

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import AppliedRoute from './components/AppliedRoute';
 import Home from './containers/Home';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
+import NewNote from './containers/NewNote';
 import NotFound from './containers/NotFound';
 
 export default ({ childProps }) => 
@@ -12,5 +13,6 @@ export default ({ childProps }) =>
         <AppliedRoute path='/' exact component={Home} props={childProps} />
         <AppliedRoute path='/login' exact component={Login} props={childProps} />
         <AppliedRoute path='/signup' exact component={Signup} props={childProps} />
+        <AppliedRoute path='/notes/new' exact component={NewNote} props={childProps} />
         <Route component={NotFound} />
     </Switch>;
