3f53174db7962167ec2b8d5dff0d63be121a0106
theperfectfuel <robert@theperfectfuel.com>
2017-10-21 01:31:33 -0700

    Finished listing all notes

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import AppliedRoute from './components/AppliedRoute';
 
 import Home from './containers/Home';
+import Notes from './containers/Notes';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import NewNote from './containers/NewNote';
@@ -14,5 +15,6 @@ export default ({ childProps }) =>
         <AppliedRoute path='/login' exact component={Login} props={childProps} />
         <AppliedRoute path='/signup' exact component={Signup} props={childProps} />
         <AppliedRoute path='/notes/new' exact component={NewNote} props={childProps} />
+        <AppliedRoute path='/notes/:id' exact component={Notes} props={childProps} />
         <Route component={NotFound} />
     </Switch>;
