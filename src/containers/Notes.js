1126494db4720983e2725e076daa8030ae02b21b
theperfectfuel <robert@theperfectfuel.com>
2017-10-22 02:02:31 -0700

    Editing notes is working

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { invokeApig } from '../libs/awsLib';
+import { invokeApig, s3Upload } from '../libs/awsLib';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import LoaderButton from '../components/LoaderButton';
 import config from '../config';
@@ -55,7 +55,17 @@ export default class Notes extends Component {
         this.file = event.target.files[0];
     }
       
+    saveNote(note) {
+        return invokeApig({
+          path: `/notes/${this.props.match.params.id}`,
+          method: "PUT",
+          body: note
+        });
+    }
+      
     handleSubmit = async event => {
+        let uploadedFilename;
+      
         event.preventDefault();
       
         if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
@@ -64,8 +74,25 @@ export default class Notes extends Component {
         }
       
         this.setState({ isLoading: true });
-    }
       
+        try {
+          if (this.file) {
+            uploadedFilename = (await s3Upload(this.file))
+              .Location;
+          }
+      
+          await this.saveNote({
+            ...this.state.note,
+            content: this.state.content,
+            attachment: uploadedFilename || this.state.note.attachment
+          });
+          this.props.history.push("/");
+        } catch (e) {
+          alert(e);
+          this.setState({ isLoading: false });
+        }
+    }
+
     handleDelete = async event => {
         event.preventDefault();
       
